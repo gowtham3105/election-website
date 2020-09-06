86b869e14e2b112f583bd83fb7541497b28b6c53
IamODJ <omkarj1422001@gmail.com>
2020-09-06 15:12:29 +0530

    added Cards with animations

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import CardList from './Components/Cardlist/CardList';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+
+  render() {
+    const { robots } = this.state;
+    return (<CardList robots={robots} />);
+
+  }
 }
 
 export default App;
