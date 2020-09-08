6f642326679626a66dbeb5de71319206607ab453
IamODJ <omkarj1422001@gmail.com>
2020-09-09 00:24:55 +0530

    added sample election and countdown

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,64 @@
 import React from 'react';
 import CardList from './Components/Cardlist/CardList';
+import Electioncard from './Components/Electioncard/Electioncard';
+import Countdown from 'react-countdown';
 import './App.css';
+import Flip from "./Flip";
+import Tick from '@pqina/flip';
+import './Tick.css'
+
+const renderer = ({ days, hours, minutes, seconds, completed }) => {
+  let daysgg=days.toString();
+  let hoursgg=hours.toString();
+  let minutesgg=minutes.toString();
+  let secondsgg=seconds.toString();
+  let day1= days.toString().length === 1 ? "0" : daysgg[0];
+  let day2= days.toString().length === 1 ? days : daysgg[1];
+  let hour1= hours.toString().length === 1 ? "0" : hoursgg[0];
+  let hour2= hours.toString().length === 1 ? hours : hoursgg[1];
+  let min1= minutes.toString().length === 1 ? "0" : minutesgg[0];
+  let min2= minutes.toString().length === 1 ? minutes : minutesgg[1];
+  let sec1= seconds.toString().length === 1 ? "0" : secondsgg[0];
+  let sec2= seconds.toString().length === 1 ? seconds : secondsgg[1];
+
+  if (completed) {
+   return null;
+  } else {
+    return (
+      <div className='enclose'>
+      <div className='timerbox'>  
+      <Flip value={day1} />
+      <Flip value={day2} />
+        <div className='colon'>
+      :</div>
+      <Flip value={hour1} />
+      <Flip value={hour2} />
+        <div className='colon'>
+      :</div>
+      <Flip value={min1} />
+      <Flip value={min2} />
+        <div className='colon'>
+      :</div>
+      <Flip value={sec1} />
+      <Flip value={sec2} />
+    </div>
+    <div className='datevals'>
+    <div className='tex'>Days</div>
+        <div>
+      </div>
+      <div className='tex'>Hours</div>
+        <div>
+      </div>
+      <div className='tex' >Minutes</div>
+        <div>
+      </div>
+      <div className='tex' >Seconds</div>
+    </div>
+    </div>
+
+    );
+  }
+};
 
 class App extends React.Component {
   constructor() {
@@ -10,6 +68,7 @@ class App extends React.Component {
     }
   }
 
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
@@ -17,10 +76,17 @@ class App extends React.Component {
   }
 
   render() {
-    const { robots } = this.state;
-    return (<CardList robots={robots} />);
+        // const { robots } = this.state;
 
-  }
+    return (<Countdown
+    date={Date.now() + 1200000}
+    renderer={renderer}>
+
+  </Countdown>);
+
+    // return (<Electioncard/>);
+
+  } 
 }
 
 export default App;
